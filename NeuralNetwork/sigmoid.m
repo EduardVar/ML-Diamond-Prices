3d0710034621f855e9ae30704e3b3aef6c2f9b5a
EduardVar <eduardvarshavsky@gmail.com>
2019-09-07 17:08:47 -0400

    Reverts to using sigmoid for ALL forward props
    Other than the final forward movement where activation isn't used (to allow for a number > 1

--- NeuralNetwork/sigmoid.m
+++ NeuralNetwork/sigmoid.m
@@ -2,7 +2,7 @@ function g = sigmoid(z)
 %SIGMOID Compute sigmoid functoon
 %   J = SIGMOID(z) computes the sigmoid of z.
 
-%g = 1.0 ./ (1.0 + exp(-z)); % Normal Sigmoid
-g = tanh(z);                % Tanh activation function
+g = 1.0 ./ (1.0 + exp(-z)); % Normal Sigmoid
+%g = tanh(z);                % Tanh activation function
 %g = max(z, [], 2);          % ReLU activation function
 end
--- NeuralNetwork/sigmoidGradient.m
+++ NeuralNetwork/sigmoidGradient.m
@@ -6,8 +6,8 @@ function g = sigmoidGradient(z)
 %   vector. In particular, if z is a vector or matrix, you should return
 %   the gradient for each element.
 
-%g = sigmoid(z) .* (1 - sigmoid(z)); % Sigmoid gradient Descent
-g = 1 - (sigmoid(z)).^2;            % Tanh descent
+g = sigmoid(z) .* (1 - sigmoid(z)); % Sigmoid gradient Descent
+%g = 1 - (sigmoid(z)).^2;            % Tanh descent
 %{
 if z < 0                            % ReLU gradient descent
     g = 0;
